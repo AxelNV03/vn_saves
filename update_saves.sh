ac21b0c01e1bca42eddd12e045c17776e3d563f5
Nava <axelnava3105@gmail.com>
2025-08-15 00:47:05 -0500

    Actualización de archivos de guardado

--- update_saves.sh
+++ update_saves.sh
@@ -27,7 +27,7 @@ sync_saves_to_git() {
     # Sincronizar carpetas del repositorio
     echo -e "\nSincronizando saves...\n"
     for dir in "${novelas[@]}"; do
-        printf '📂 %-25s ' "$dir"
+        printf '📂 %-26s ' "$dir"
         if [[ -d "$VN_PATH/$dir/data" ]]; then
             if [[ -d "$VN_PATH/$dir/data/game/saves" ]]; then
                 if rsync -a "$VN_PATH/$dir/data/game/saves/" "$GIT_PATH/$dir/" > /dev/null 2>&1; then
